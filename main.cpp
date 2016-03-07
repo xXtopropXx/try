dc850943500444dc77d3123cb9ee88a031ecc840
Ofir Hershko <odh1234321@gmail.com>
2016-03-07 16:57:16 +0200

    prints new lines

--- main.cpp
+++ main.cpp
@@ -4,5 +4,7 @@ using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
+    cout << "This is my first new branch?" << endl;
+    cout << "my name is Ofir Herhsko" << endl;
     return 0;
 }
